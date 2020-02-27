f967046af16a15d308bb4a3240d0f81b1cc42def
godric <miaochengod@outlook.com>
2020-02-27 23:35:09 +0800

    rbac基础service

---
+++ tiangong-component/tiangong-component-rbac/src/main/java/com/kaizhuo/component/rbac/service/DeptService.java
@@ -0,0 +1,17 @@
+package com.kaizhuo.component.rbac.service;
+
+import com.kaizhuo.component.rbac.model.domain.Dept;
+import com.kaizhuo.core.service.BaseService;
+
+/**
+ * @program: tiangong
+ * @package: com.kaizhuo.component.rbac.service
+ * @description:
+ * @author: miaochen
+ * @create: 2020-02-27 23:32
+ * @copyright: CopyRight (c) 2020
+ * @version: 1.0.0
+ * @modified: miaochen
+ **/
+public interface DeptService extends BaseService<Dept> {
+}
---
+++ tiangong-component/tiangong-component-rbac/src/main/java/com/kaizhuo/component/rbac/service/MenuService.java
@@ -0,0 +1,17 @@
+package com.kaizhuo.component.rbac.service;
+
+import com.kaizhuo.component.rbac.model.domain.Menu;
+import com.kaizhuo.core.service.BaseService;
+
+/**
+ * @program: tiangong
+ * @package: com.kaizhuo.component.rbac.service
+ * @description:
+ * @author: miaochen
+ * @create: 2020-02-27 23:33
+ * @copyright: CopyRight (c) 2020
+ * @version: 1.0.0
+ * @modified: miaochen
+ **/
+public interface MenuService extends BaseService<Menu> {
+}
